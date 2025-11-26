851597d7e1bd0c72adad75e8a09a454e7be0da20
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2025-11-26 11:17:12 +0900

    blue green study

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello, World!");
+app.get('/health', (req, res) => {
+    const now = new Date();
+    const hh = String(now.getHours()).padStart(2, '0');
+    const mm = String(now.getMinutes()).padStart(2, '0');
+    const ss = String(now.getSeconds()).padStart(2, '0');
+    const timeString = `${hh}:${mm}:${ss}`;
+
+    res.status(200).send(`OK - ${timeString} \n`);
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-})
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
